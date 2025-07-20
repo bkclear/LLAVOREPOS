f8a9a94e649015bb0edec5e769ab7c4b42170e1a
bkclear <35098221+bkclear@users.noreply.github.com>
2025-07-21 01:36:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,31 @@
-// Define a cache name
-const CACHE_NAME = 'llavorepos-cache-v3'; // Using v3 to force an update
-
-// List the ONLY files we need to cache
+const CACHE_NAME = 'pos-cache-v1';
 const urlsToCache = [
   '/LLAVOREPOS/',
   '/LLAVOREPOS/index.html',
+  '/LLAVOREPOS/manifest.json',
   '/LLAVOREPOS/icons/icon-192x192.png',
   '/LLAVOREPOS/icons/icon-512x512.png'
 ];
 
-// Install event
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-// Fetch event
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
 
-// Clean up old caches
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
